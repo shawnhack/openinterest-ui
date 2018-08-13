91ef138afbdbb43db29bdb3e4749c721df7e55b5
Shawn Hack <shawnhack@gmail.com>
2018-08-13 16:49:35 -0400

    Initial commit

---
+++ src/main/java/live/openinterest/ui/OpenInterestApplication.java
@@ -0,0 +1,17 @@
+package live.openinterest.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenInterestApplication {
+
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(OpenInterestApplication.class);
+    }
+
+}

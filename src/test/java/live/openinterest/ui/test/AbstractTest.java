9d952d923682325f3513afb1e6edf858d73c9ea7
Shawn Hack <shawnhack@gmail.com>
2018-08-21 21:03:22 -0400

    Fixed unit tests

--- src/test/java/live/openinterest/ui/test/AbstractTest.java
+++ src/test/java/live/openinterest/ui/test/AbstractTest.java
@@ -1,14 +1,11 @@
 package live.openinterest.ui.test;
 
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @ActiveProfiles("test")
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public abstract class AbstractTest {
